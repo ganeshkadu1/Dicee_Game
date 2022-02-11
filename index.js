cbbfea7d2b3b120909a619f595f1809b703a0778
ganeshkadu1 <ganeshkadu20@gmail.com>
2022-02-12 01:01:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6, 2) + 1;
+
+var imagePath1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", imagePath1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6, 2) + 1;
+
+var imagePath2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", imagePath2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!✨";
+}
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!✨";
+}
+
+if (randomNumber1 == randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
