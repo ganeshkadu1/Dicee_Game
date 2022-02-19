f162855ac555006fff39448faa8b09feb55f7bb9
ganeshkadu1 <ganeshkadu20@gmail.com>
2022-02-19 11:46:42 +0530

    Added a Comment

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6, 2) + 1;
 
+//Dice 1
 var imagePath1 = "images/dice" + randomNumber1 + ".png";
 document.querySelector(".img1").setAttribute("src", imagePath1);
 
 var randomNumber2 = Math.floor(Math.random() * 6, 2) + 1;
 
+//Dice 2
 var imagePath2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", imagePath2);
 
